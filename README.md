10482f0758faa1256d9b7e1f4d32fea9fce0aa45
John Peters <peters.johnathan@gmail.com>
2018-11-28 12:04:44 -0500

    final product

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # LIRI
 Assignment #8 - Node.JS
+This project uses Node to interact with movie, song, and concert venue APIs.
+
+## Getting Started
+
+Clone this repo to your desktop and run npm install to install all the dependencies.
+
+You might want to look into config.json to make change the port you want to use and set up a SSL certificate.
+
+### Installing
+
+After you clone this repo to your desktop, go to its root directory and run npm install to install its dependencies.
+
+A step by step series of examples that tell you how to get a development env running
+
+## Commands
+begin search by typing "node liri.js " followed by one of the commands below and your desired search request.
+
+* Search Concert Venue API - "concert-this"
+* Search Song API - "spotify-this-song"
+* Search Movie API - "movie-this"
+* Search With FS - "do-what-it-says"
+
+## Built With
+
+* [Node Spotify API](https://www.npmjs.com/package/node-spotify-api) - API used to search song data
+* [OMDb API](http://www.omdbapi.com/) - The OMDb API is a RESTful web service to obtain movie information. 
+* [Request - Simplified HTTP client](https://www.npmjs.com/package/request) - Request is designed to be the simplest way possible to make http calls. It supports HTTPS and follows redirects by default.
+* [Bands In Town](https://manager.bandsintown.com/support/bandsintown-api) - The Bandsintown API is designed for enterprise partners and artists with websites, media players, and/or mobile applications that would like to list an artist’s events and provide their users with the ability to buy tickets and RSVP to these events.
+
+## Documentation
+[Spotify](https://developer.spotify.com/documentation/web-api/)
+[OMDB](http://www.omdbapi.com/)
+[Bands In Town](https://app.swaggerhub.com/apis/Bandsintown/PublicAPI/3.0.0)
+
+## Authors
+
+* **Johnathan Peters** - [Jppeters16](https://github.com/jppeters16)
--- liri.js
+++ liri.js
@@ -3,31 +3,28 @@ require("dotenv").config();
 var keys = require("./keys.js");
 var fs = require("fs");
 var request = require("request");
+var moment = require('moment');
 var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
 var commands = process.argv[2];
-
 //allows spaces in the users input
-var input = "";
-for (i = 3; i < process.argv.length; i++) {
-  input += process.argv[i] + " ";
-}
+var input = process.argv.slice(3).join("+");
 
 switch (commands) {
   case "concert-this":
-    concertThis();
+    concertThis(input);
     break;
 
   case "spotify-this-song":
-    spotifyThisSong();
+    spotifyThisSong(input);
     break;
 
   case "movie-this":
-    movieThis();
+    movieThis(input);
     break;
 
   case "do-what-it-says":
-    doWhatItSays();
+    doWhatItSays(input);
     break;
 }
 
@@ -36,37 +33,50 @@ function concertThis(artist) {
     "https://rest.bandsintown.com/artists/" +
     artist +
     "/events?app_id=codingbootcamp";
+    request(query, function(err, res) {
+        if (err) {
+          console.log("Error occurred: " + err);
+          return err;
+        } else {
+
+          const object1 = JSON.parse(res.body);
+          // console.log(object1[0].venue.name);
+          // console.log(body.body);
+
+
   console.log(
     "\nNext Venue: " +
-      body.venue.name +
-      "at " +
-      body.venue.city +
+      object1[0].venue.name +
+      " at " +
+      object1[0].venue.city +
       ", " +
-      body.venue.region
+      object1[0].venue.region
   );
-  console.log("\nDate of Event: " + moment(body.datetime).format("MM DD YYYY"));
+  console.log("\nDate of Event: " + moment(object1[0].datetime).format("MMM Do YYYY"));
+}
+    });
 }
 
 function spotifyThisSong(song) {
   if (song === "") {
-    search = "The Sign";
+    song = "The Sign";
   }
-  spotify.search({ type: "track", query: song, limit: 1 }, function(
+  spotify.search({ type: "track", query: song}, function(
     err,
-    data,
-    response
+    data
   ) {
     if (err) {
-      console.log(err);
+      console.log("Error occurred: " + err);
     } else {
-      console.log("\nArtist(s) " + data.tracks.items[0].artists[0].name);
       console.log("\nSong Name: " + data.tracks.items[0].name);
+      console.log("\nArtist(s) " + data.tracks.items[0].artists[0].name);
       console.log("\nPreview URL: " + data.tracks.items[0].preview_url);
       console.log("\nAlbum: " + data.tracks.items[0].album.name);
     }
   });
 }
 function movieThis(movie) {
+    console.log(movie);
   if (movie === "") {
     console.log("\nSince you didn't submit a movie, we'll recommend one!");
     console.log(
@@ -78,30 +88,59 @@ function movieThis(movie) {
 
   var movieURL =
     "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
-  request(movieURL, function(err, response, body) {
+  request(movieURL, function(err, res) {
     if (err) {
-      console.log(err);
+      console.log("Error occurred: " + err);
       return err;
     } else {
-      console.log("\nThe movie title is: " + JSON.parse(body).Title);
-      console.log("\nThe year the movie came out is " + JSON.parse(body).Year);
+      const data2 = JSON.parse(res.body);
+      console.log(data2);
+      console.log("\nThe movie title is: " + data2.Title);
+      console.log("\nThe year the movie came out is " + data2.Year);
       console.log(
-        "\nThe IMDB Rating of the movie is " + JSON.parse(body).imdbRating
+        "\nThe IMDB Rating of the movie is " + data2.imdbRating
       );
+      Object.entries(data2.Ratings[1]).forEach(([key, value]) => {
+        console.log(`${key} ${value}`);
+      });
       console.log(
         "\nThe Rotten Tomatoes Rating of the movie is " +
-          JSON.parse(body).Ratings[1]
+          data2.Ratings[1]
       );
       console.log(
-        "\nThe country/countries where the movie was produced is: " +
-          JSON.parse(body).Country
+        "\nThe country/countries where the movie was produced is/are: " +
+          data2.Country
       );
       console.log(
-        "\nThe language(s) of the movie is/are: " + JSON.parse(body).Language
+        "\nThe language(s) of the movie is/are: " + data2.Language
       );
-      console.log("\nThe plot is: " + JSON.parse(body).Plot);
-      console.log("\nThe actors are: " + JSON.parse(body).Actors);
+      console.log("\nThe plot is: " + data2.Plot);
+      console.log("\nThe actors are: " + data2.Actors);
       return;
+    }
+  });
+}
+
+function doWhatItSays() {
+  fs.readFile("random.txt", "utf8", function (err, data) {
+      if (err) {
+        console.log("Error occurred: " + err);
+      return err;
+    } else {
+      var dataArray = data.split(",");
+
+      if (dataArray[0] === "spotify-this-song") {
+        var songName = dataArray.slice(1).join("+");
+        spotifyThisSong(songName);
+      }
+      if (dataArray[0] === "movie-this") {
+        var movieName = dataArray.slice(1).join("+");
+        movieThis(movieName);
+      }
+      if (dataArray[0] === "concert-this") {
+        var artistName = dataArray.slice(1).join("+");
+        concertThis(artistName);
+      }
     }
   });
 }
